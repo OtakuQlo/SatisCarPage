6c4a8f8552f887d050224573022faa2c0be4aab5
OtakuQlo_ <manuelenrriquerivera@gmail.com>
2023-11-20 19:09:12 -0300

    Qr

--- Satiscar/apk/static/apk/js/tipodispositivo.js
+++ Satiscar/apk/static/apk/js/tipodispositivo.js
@@ -4,7 +4,7 @@ if (isMobile) {
     document.getElementById("link").innerHTML = '<a href="https://drive.google.com/file/d/1i707vrcLlpw2PFkXrZijBfj0N6YbH8E9/view?usp=sharing" class="my-button">¡Descarga ya SatisCar!</a>';
     
 } else {
-    document.getElementById("link").innerHTML = '<img src="../../static/apk/img/qrapk.jpeg" alt="" class="foto">';
+    document.getElementById("link").innerHTML = '<img src="../../static/apk/img/APK.png" alt="" class="foto">';
 }
 
 
